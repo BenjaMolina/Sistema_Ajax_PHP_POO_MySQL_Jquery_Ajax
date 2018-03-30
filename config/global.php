35a5e86c6f2a14d4b9ad3f7ccc66448efaf794c7
BenjaMolina <molinasb.94@gmail.com>
2018-03-30 15:07:02 -0600

    Variables globales

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+    //IP DEL SERVIDOR DE BASE DE DATOS
+    define("DB_HOST","localhost");
+
+    //NOMBRE DE LA BASE DE DATOS
+    define("DB_NAME","dbsistema");
+
+    //USUARIO DE LA BASE DE DATOS
+    define("DB_USERNAME","root");
+
+    //CONTRASEÑA DEL USUARIO DE LA BASE DE DATOS
+    define("DB_PASSWORD","");
+
+    //CODIFICACION DE LOS CARACTERES
+    define("DB_ENCODE","utf8");
+
+    //CONSTANTE COMO NOMBRE DEL PROYECTO
+    define("PRO_NOMBRE","ITVentas");
+
+    
+?>
