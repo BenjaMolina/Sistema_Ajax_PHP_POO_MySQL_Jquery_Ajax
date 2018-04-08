8f540ecc5beaa2b5ea6f6e6655381338eacac30f
BenjaMolina <molinasb.94@gmail.com>
2018-04-08 17:22:35 -0500

    Funciones CRUD Persona mediante Ajax

---
+++ ajax/persona.php
@@ -0,0 +1,89 @@
+<?php
+    
+    require_once '../modelos/Persona.php';
+
+    $persona = new Persona();
+
+    $idpersona=isset($_POST["idpersona"])? limpiarCadena($_POST["idpersona"]):"";
+    $tipo_persona=isset($_POST["tipo_persona"])? limpiarCadena($_POST["tipo_persona"]):"";
+    $nombre=isset($_POST["nombre"])? limpiarCadena($_POST["nombre"]):"";
+    $tipo_documento=isset($_POST["tipo_documento"])? limpiarCadena($_POST["tipo_documento"]):"";
+    $direccion=isset($_POST["direccion"])? limpiarCadena($_POST["direccion"]):"";
+    $telefono=isset($_POST["telefono"])? limpiarCadena($_POST["telefono"]):"";
+    $email=isset($_POST["email"])? limpiarCadena($_POST["email"]):"";
+
+    switch($_GET["op"])
+    {
+        case 'guardaryeditar':
+            if (empty($idpersona)){
+                $rspta=$persona->insertar($tipo_persona,$nombre,$tipo_documento,$direccion,$telefono,$email);
+                echo $rspta ? "Persona registrada" : "Persona no se pudo registrar";
+            }
+            else {
+                $rspta=$persona->editar($idpersona,$tipo_persona,$nombre,$tipo_documento,$direccion,$telefono,$email);
+                echo $rspta ? "Persona actualizada" : "Persona no se pudo actualizar";
+            } 
+        break;
+
+        case 'eliminar':
+                $rspta = $persona->eliminar($idpersona);
+                echo $rspta ? "Persona eliminada" : "Persona no se pudo eliminar";
+        break;
+
+        case 'mostrar':
+            $rspta = $persona->mostrar($idpersona);
+            echo json_encode($rspta);
+        break;
+
+        case 'listarp':
+            $rspta = $persona->listarp();
+            $data = Array();
+            while ($reg = $rspta->fetch_object()) {
+                $data[] = array(
+                    "0"=>
+                        '<button class="btn btn-warning" onclick="mostrar('.$reg->idpersona.')"><li class="fa fa-pencil"></li></button>'.
+                        ' <button class="btn btn-danger" onclick="eliminar('.$reg->idpersona.')"><li class="fa fa-trash"></li></button>'
+                        ,
+                    "1"=>$reg->nombre,
+                    "2"=>$reg->tipo_documento,
+                    "3"=>$reg->num_documento,
+                    "4"=>$reg->telefono,
+                    "5"=> $reg->email
+                );
+            }
+            $results = array(
+                "sEcho"=>1, //Informacion para el datable
+                "iTotalRecords" =>count($data), //enviamos el total de registros al datatable
+                "iTotalDisplayRecords" => count($data), //enviamos el total de registros a visualizar
+                "aaData" =>$data
+            );
+            echo json_encode($results);
+        break;
+
+        case 'listarc':
+            $rspta = $persona->listarc();
+            $data = Array();
+            while ($reg = $rspta->fetch_object()) {
+                $data[] = array(
+                    "0"=>
+                        '<button class="btn btn-warning" onclick="mostrar('.$reg->idpersona.')"><li class="fa fa-pencil"></li></button>'.
+                        ' <button class="btn btn-danger" onclick="eliminar('.$reg->idpersona.')"><li class="fa fa-trash"></li></button>'
+                        ,
+                    "1"=>$reg->nombre,
+                    "2"=>$reg->tipo_documento,
+                    "3"=>$reg->num_documento,
+                    "4"=>$reg->telefono,
+                    "5"=> $reg->email
+                );
+            }
+            $results = array(
+                "sEcho"=>1, //Informacion para el datable
+                "iTotalRecords" =>count($data), //enviamos el total de registros al datatable
+                "iTotalDisplayRecords" => count($data), //enviamos el total de registros a visualizar
+                "aaData" =>$data
+            );
+            echo json_encode($results);
+        break;
+    }
+
+?>
