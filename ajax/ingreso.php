63bc91295b69ec7882a4d2fa8185b0335be29784
BenjaMolina <molinasb.94@gmail.com>
2018-04-15 15:11:34 -0500

    Ingresos - compras Funciones listar,editar,mostrar,anular

---
+++ ajax/ingreso.php
@@ -0,0 +1,74 @@
+<?php
+    
+    require_once '../modelos/Ingreso.php';
+
+    $ingreso = new Ingreso();
+
+    $idingreso=isset($_POST["idingreso"])? limpiarCadena($_POST["idingreso"]):"";
+    $idproveedor=isset($_POST["idproveedor"])? limpiarCadena($_POST["idproveedor"]):"";
+    $idusuario= $_SESSION['idusuario'];
+    $tipo_comprobante=isset($_POST["tipo_comprobante"])? limpiarCadena($_POST["tipo_comprobante"]):"";
+    $serie_comprobante=isset($_POST["serie_comprobante"])? limpiarCadena($_POST["serie_comprobante"]):"";
+    $num_comprobante=isset($_POST["num_comprobante"])? limpiarCadena($_POST["num_comprobante"]):"";
+    $fecha_hora=isset($_POST["fecha_hora"])? limpiarCadena($_POST["fecha_hora"]):"";
+    $impuesto=isset($_POST["impuesto"])? limpiarCadena($_POST["impuesto"]):"";
+    $total_compra=isset($_POST["total_compra"])? limpiarCadena($_POST["total_compra"]):"";
+
+    $idarticulo = $_POST['idarticulo'];
+    $cantidad = $_POST['cantidad'];
+    $precio_compra = $_POST['precio_compra'];
+    $precio_venta = $_POST['precio_venta'];
+
+
+    switch($_GET["op"])
+    {
+        case 'guardaryeditar':
+            if (empty($idingreso)){
+                $rspta=$ingreso->insertar($idproveedor,$idusuario,$tipo_comprobante,$serie_comprobante,$num_comprobante,$fecha_hora,$impuesto,$total_compra,$idarticulo,$cantidad,$precio_compra,$precio_venta);
+                echo $rspta ? "Ingreso registrado" : "Ingreso no se pudo registrar";
+            }
+        break;
+
+        case 'anular':
+                $rspta = $ingreso->anular($idingreso);
+                echo $rspta ? "Ingreso anulado" : "Ingreso no se pudo anular";
+        break;
+
+        case 'mostrar':
+            $rspta = $ingreso->mostrar($idingreso);
+            echo json_encode($rspta);
+        break;
+
+        case 'listar':
+            $rspta = $ingreso->listar();
+            $data = Array();
+            while ($reg = $rspta->fetch_object()) {
+                $data[] = array(
+                    "0"=> ($reg->estado == 'Aceptado') ? 
+                        '<button class="btn btn-warning" onclick="mostrar('.$reg->idingreso.')"><li class="fa fa-pencil"></li></button>'.
+                        ' <button class="btn btn-danger" onclick="anular('.$reg->idingreso.')"><li class="fa fa-close"></li></button>'
+                        :
+                        '<button class="btn btn-warning" onclick="mostrar('.$reg->idingreso.')"><li class="fa fa-pencil"></li></button>',
+                    "1"=>$reg->fecha,
+                    "2"=>$reg->proveedor,
+                    "3"=>$reg->usuario,
+                    "4"=>$reg->tipo_comprobante,
+                    "5"=>$reg->serie_comprobante.'-'.$reg->num_comprobante,
+                    "6"=>$reg->total_compra,
+                    "7"=>($reg->condicion=='Aceptado') ?
+                         '<span class="label bg-green">Aceptado</span>'
+                         :      
+                         '<span class="label bg-red">Anulado</span>'
+                );
+            }
+            $results = array(
+                "sEcho"=>1, //Informacion para el datable
+                "iTotalRecords" =>count($data), //enviamos el total de registros al datatable
+                "iTotalDisplayRecords" => count($data), //enviamos el total de registros a visualizar
+                "aaData" =>$data
+            );
+            echo json_encode($results);
+        break;
+    }
+
+?>
