1a6d9bf54fe0b3abb39ca8e870291d14f23abf18
BenjaMolina <molinasb.94@gmail.com>
2018-04-15 16:45:29 -0500

    Ingreso formulario compra y detalles

--- vistas/ingreso.php
+++ vistas/ingreso.php
@@ -59,21 +59,77 @@
                           </tfoot>
                         </table>
                     </div>
-                    <div class="panel-body" style="height: 400px;" id="formularioregistros">
+                    <div class="panel-body" id="formularioregistros">
                         <form name="formulario" id="formulario" method="POST">
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <label>Nombre:</label>
-                            <input type="hidden" name="idcategoria" id="idcategoria">
-                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
+                          <div class="form-group col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                            <label>Proveedor(*):</label>
+                            <input type="hidden" name="idingreso" id="idcategoria">
+                            <select name="idproveedor" id="idproveedor" class="form-control selectpicker" data-live-search="true" required></select>
+                          </div>
+                          <div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <label>Fecha(*):</label>
+                            <input type="date" class="form-control" name="fecha_hora" id="fecha_hora" required>
                           </div>
                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <label>Descripción:</label>
-                            <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripción">
+                            <label>Tipo Comprobante(*):</label>
+                            <select class="form-control selectpicker" name="tipo_comprobante" id="tipo_comprobante" required>
+                              <option value="Boleta">Boleta</option>
+                              <option value="Factura">Factura</option>
+                              <option value="Ticket">Ticket</option>
+                            </select>
                           </div>
-                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
+                          <div class="form-group col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                            <label>Serie:</label>
+                            <input type="text" class="form-control" name="serie_comprobante" id="serie_comprobante" maxlength="7" placeholder="Serie">
+                          </div>
+                          <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
+                            <label>Numero:</label>
+                            <input type="text" class="form-control" name="num_comprobante" id="num_comprobante" maxlength="10" placeholder="Numero">
+                          </div>
+                          <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
+                            <label>Impuesto:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required>
+                          </div>
 
-                            <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                          <div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                              <a data-toggle="modal" href="#myModal" >
+                                <button id="btnAgregarArt" type="button" class="btn btn-primary">
+                                  <span class="fa fa-plus"></span>
+                                  Agregar Articulos
+                                </button>
+                              </a>
+                          </div>
+
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                              <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                                <thead style="background-color:#A9D0F5">
+                                  <th>Opciones</th>
+                                  <th>Articulos</th>
+                                  <th>Cantidad</th>
+                                  <th>Precio Compra</th>
+                                  <th>Precio Venta</th>
+                                  <th>Subtotal</th>
+                                </thead>
+                                <tfoot>
+                                  <th>TOTAL</th>
+                                  <th></th>
+                                  <th></th>
+                                  <th></th>
+                                  <th></th>
+                                  <th>
+                                    <h4 class="idtotal">$ 0.00</h4>
+                                    <input type="hidden" name="total_compra" id="total_compra">
+                                  </th>
+                                </tfoot>
+                                <tbody>
+
+                                </tbody>
+                              </table>
+                          </div>
+                          
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12" id="guardar">
+                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
+                            <button class="btn btn-danger" onclick="cancelarform()" type="button" id="btnCancelar"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                           </div>
                         </form>
                     </div>
