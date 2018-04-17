660bfa509455f2a60eb8c1e5626c39cb10b34816
BenjaMolina <molinasb.94@gmail.com>
2018-04-17 16:39:06 -0500

    Cliente mantenimiento CRUD

---
+++ vistas/cliente.php
@@ -0,0 +1,126 @@
+<?php
+    //Activacion de almacenamiento en buffer
+    ob_start();
+    //iniciamos las variables de session
+    session_start();
+  
+    if(!isset($_SESSION["nombre"]))
+    {
+      header("Location: login.html");
+    }
+  
+    else  //Agrega toda la vista
+    {
+      require 'header.php';
+
+      if($_SESSION['ventas'] == 1)
+      {
+?>
+
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Cliente <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
+                        <div class="box-tools pull-right">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body table-responsive" id="listadoregistros">
+                        <table id="tblistado" class="table table-striped table-bordered table-condensed table-hover">
+                          <thead>
+                            <th>Opciones</th>
+                            <th>Nombre</th>
+                            <th>Documento</th>
+                            <th>Numero Doc.</th>
+                            <th>Telefono</th>
+                            <th>Email</th>
+                          </thead>
+                          <tbody>
+
+                          </tbody>
+                          <tfoot>
+                            <th>Opciones</th>
+                            <th>Nombre</th>
+                            <th>Documento</th>
+                            <th>Numero Doc.</th>
+                            <th>Telefono</th>
+                            <th>Email</th>
+                          </tfoot>
+                        </table>
+                    </div>
+                    <div class="panel-body" style="" id="formularioregistros">
+                        <form name="formulario" id="formulario" method="POST">
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Nombre:</label>
+                            <input type="hidden" name="idpersona" id="idpersona">
+                            <input type="hidden" name="tipo_persona" id="tipo_persona" value="Cliente">
+                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="100" placeholder="Nombre Cliente" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Tipo Documento:</label>
+                                <select name="tipo_documento" id="tipo_documento" class="form-control select-picker" required>
+                                    <option value="DNI">DNI</option>
+                                    <option value="RUC">RUC</option>
+                                    <option value="CEDULA">CEDULA</option>
+                                </select>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Numero Documento:</label>
+                            <input type="text" class="form-control" name="num_documento" id="num_documento" maxlength="20" placeholder="Num. Documento" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Direccion:</label>
+                            <input type="text" class="form-control" name="direccion" id="direccion" maxlength="70" placeholder="Direccion" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Telefono:</label>
+                            <input type="text" class="form-control" name="telefono" id="telefono" maxlength="20" placeholder="Telefono" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Email:</label>
+                            <input type="email" class="form-control" name="email" id="email" maxlength="50" placeholder="Email" required>
+                          </div>
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
+
+                            <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                          </div>
+                        </form>
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+
+
+<?php
+
+  } //Llave de la condicion if de la variable de session
+
+  else
+  {
+    require 'noacceso.php';
+  }
+
+  require 'footer.php';
+?>
+
+<script src="./scripts/cliente.js"></script>
+
+
+<?php
+
+  }
+  ob_end_flush(); //liberar el espacio del buffer
+?>
---
+++ vistas/scripts/cliente.js
@@ -0,0 +1,157 @@
+var tabla;
+
+//Funcion que se ejecuta al inicio
+function init()
+{
+    mostrarform(false);
+    listar();
+
+    $("#formulario").on("submit",function(e)
+    {
+        guardaryeditar(e);
+    })
+}
+
+//funcion limpiar
+function limpiar()
+{
+    $("#nombre").val("");
+    $("#num_documento").val("");
+    $("#direccion").val("");
+    $("#telefono").val("");
+    $("#email").val("");
+    $("#idpersona").val("");
+}
+
+//funcion mostrar formulario
+function mostrarform(flag)
+{
+    limpiar();
+
+    if(flag)
+    {
+        $("#listadoregistros").hide();
+        $("#formularioregistros").show();
+        $("#btnGuardar").prop("disabled",false);
+        $("#btnagregar").hide();
+    }
+    else
+    {
+        $("#listadoregistros").show();
+        $("#formularioregistros").hide();
+        $("#btnagregar").show();
+    }
+}
+
+//Funcion cancelarform
+function cancelarform()
+{
+    limpiar();
+    mostrarform(false);
+}
+
+//Funcion listar
+function listar()
+{
+    tabla = $('#tblistado')
+        .dataTable(
+            {
+                "aProcessing":true, //Activamos el procesamiento del datatables
+                "aServerSide":true, //Paginacion y filtrado realizados por el servidor
+                dom: "Bfrtip", //Definimos los elementos del control de tabla
+                buttons:[
+                    'copyHtml5',
+                    'excelHtml5',
+                    'csvHtml5',
+                    'pdf'
+                ],
+                "ajax":{
+                    url: '../ajax/persona.php?op=listarc',
+                    type: "get",
+                    dataType:"json",
+                    error: function(e) {
+                        console.log(e.responseText);
+                    }
+                },
+                "bDestroy": true,
+                "iDisplayLength": 5, //Paginacion
+                "order": [[0,"desc"]] //Ordenar (Columna, orden)
+            
+            })
+        .DataTable();
+}
+
+//funcion para guardar o editar
+function guardaryeditar(e)
+{
+    e.preventDefault(); //No se activará la acción predeterminada del evento
+	$("#btnGuardar").prop("disabled",true);
+    var formData = new FormData($("#formulario")[0]);
+    
+    $.ajax({
+        url: "../ajax/persona.php?op=guardaryeditar",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function(datos)
+        {
+            //console.log("succes");
+            bootbox.alert(datos);
+            mostrarform(false);
+            tabla.ajax.reload();
+        },
+        error: function(error)
+        {
+            console.log("error: " + error);
+        } 
+    });
+
+    limpiar();
+}
+
+function mostrar(idpersona)
+{
+    $.post(
+        "../ajax/persona.php?op=mostrar",
+        {idpersona:idpersona},
+        function(data,status)
+        {
+            data = JSON.parse(data);
+            mostrarform(true);
+
+            $("#nombre").val(data.nombre);
+
+            $("#tipo_documento").val(data.tipo_documento);
+            $("#tipo_documento").selectpicker('refresh');
+
+            $("#num_documento").val(data.num_documento);
+            $("#direccion").val(data.direccion);
+            $("#telefono").val(data.telefono);
+            $("#email").val(data.email);
+            $("#idpersona").val(data.idpersona);
+
+        }
+    );
+}
+
+
+function eliminar(idpersona)
+{
+    bootbox.confirm("¿Estas seguro de eliminar el Cliente?",function(result){
+        if(result)
+        {
+            $.post(
+                "../ajax/persona.php?op=eliminar",
+                {idpersona:idpersona},
+                function(e)
+                {
+                    bootbox.alert(e);
+                    tabla.ajax.reload();
+                }
+            );
+        }
+    });
+}
+
+init();
